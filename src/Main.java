f5504a62f8e72c992c1834672199dae625085898
akezhan96 <akezhan96@gmail.com>
2022-11-17 18:40:54 +0300

    123

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -31,6 +32,39 @@ public class Main {
         System.out.println("result of quadrant exp through descriminant  " + ((a-2*b)*a)/(-2*b));
 
 
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        int[] sum = nums;
+        int ans1, ans2;
+        int flag;
+        Arrays.sort(sum);
+        int x,y,mid;
+        for (int i = 0; i < sum.length; i++){
+            x = i+1;
+            y = sum.length;
+            flag = 0;
+            while (x <= y){
+                mid = (x+y)/2;
+                if (sum[x] + sum[y] > target) x = mid+1;
+                else
+                if (sum[x] + sum[y] < target) y = mid-1;
+                else
+                if (sum[x] + sum[y] == target) {
+                    ans1 = sum[x];
+                    ans2 = sum[y];
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag==1) break;
+        }
+        int posi = 1231231;
+        int posj = 1231231;
+        for (int i = 0; i < sum.length; i++){
+            if (ans1 == nums[i] && posi != 1231231) {posi = i;}
+            if (ans2 == nums[i] && posj != 1231231) {posj = i;}
+        }
+        System.out.println(posi + " " + posj);
 
     }
 
